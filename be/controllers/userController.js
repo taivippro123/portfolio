574c1c324ad1b8a950a7f93a29f116ab5f71728d
taivippro123 <phanvothanhtai1007@gmail.com>
2025-12-21 17:54:43 +0700

    fix

--- be/controllers/userController.js
+++ be/controllers/userController.js
@@ -1,4 +1,4 @@
-import User from "../models/UserModel.js";
+import User from "../models/userModel.js";
 import {comparePassword, generateToken, hashPassword, verifyToken} from "../middleware/auth.js";
 export const createUser = async (req, res) => {
   try {
