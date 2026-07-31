6e5105924e353ffbcdb4cc88fb6174aa2dd42d1c
taivippro123 <phanvothanhtai1007@gmail.com>
2026-07-31 15:41:02 +0700

    fix bugs

--- be/config/db.js
+++ be/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 dotenv.config();
 
 const connectDB = async () => {
--- be/middleware/auth.js
+++ be/middleware/auth.js
@@ -12,7 +12,7 @@ export const verifyToken = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
+        req.user = decoded;
         next();
     } catch (err) {
         res.status(401).json({ message: 'Token is not valid' });
